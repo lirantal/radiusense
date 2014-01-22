ab7ab8bc18f6ce52bc2e078e1e2a49a7b6c0add0
Liran Tal <liran.tal@gmail.com>
2014-01-22 23:05:33 +0200

    for now we will be allowing to work with only one radius server

--- app/controllers/servers.js
+++ app/controllers/servers.js
@@ -77,9 +77,10 @@ exports.delete = function(req, res) {
 
 /**
  * List of Servers
+ * @todo for the time being return only one configured server
  */
 exports.all = function(req, res) {
-    Servers.find().sort('-created').populate('user', 'name username').exec(function(err, dashboards) {
+    Servers.findOne().sort('-created').populate('user', 'name username').exec(function(err, dashboards) {
         if (err) {
             res.render('error', {
                 status: 500
