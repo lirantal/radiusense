7ca293c322324bc7b66c9fddc194c3872e69ead6
Liran Tal <liran.tal@gmail.com>
2014-04-07 23:16:29 +0300

    require a session for any access to the app

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var authorization = require('./middlewares/authorization');
+
 module.exports = function(app, passport) {
     
     // Home route
     var index = require('../controllers/index');
-    app.get('/', index.render);
+    app.get('/', authorization.requiresLogin, index.render);
 
 };
