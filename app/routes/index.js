4a2967912da555c4e59246bf963fd420beb6db1d
Liran Tal <liran.tal@gmail.com>
2014-11-13 23:48:20 +0200

    updating signin and signup routes configuration

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,15 +7,24 @@ module.exports = function(app) {
   // Users authorization related controllers
   var users = require('../../app/controllers/users.server.controller');
 
-  //app.route('/').get(core.index);
   app.get('/', function (req, res, next) {
     if (!req.isAuthenticated()) {
-      res.render('users/signin', {
-      	title: 'Sign in'
-      });
+      res.redirect('/users/signin');
     } else {
       index.render(req, res, next);
     }
+  });
+
+  app.route('/users/signin').get(function (req, res) {
+    res.render('users/signin', {
+      title: 'Sign in'
+    });
+  });
+
+  app.route('/users/signup').get(function (req, res) {
+    res.render('users/signup', {
+      title: 'Sign Up'
+    });
   });
   
 };
